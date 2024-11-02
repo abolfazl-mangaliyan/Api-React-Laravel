ae92edebcaf5fc98e414a485354bcf24d23a8f7e
“[Abolfazl <[abolfazl.mangaliyan.sh@gmail.com]>
2024-11-02 15:25:31 +0330

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2 id='h2'>یواحقیان به جز یاحقی</h2>
+        <form action="#" id='test-form'>
+          <input type="text" id='name-input' placeholder='نام خود را وارد کنید'/>
+          <button id='run'>enter</button>
+          <input type="text" id='name-output' placeholder='خروجی'/>
+        </form>
+    </div>
+  );
+}
+export default App;
